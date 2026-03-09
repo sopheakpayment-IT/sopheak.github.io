c1838c80c87fbccbb5f3027cdd52c9c22f5b6977
sopheakpayment-IT <sopheakpayment@gmail.com>
2026-03-09 11:18:49 +0700

    Add files via upload

---
+++ bootstrap/app.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use LaravelLang\Routes\Middlewares\LocalizationByCookie;
+use LaravelLang\Routes\Middlewares\LocalizationByHeader;
+use LaravelLang\Routes\Middlewares\LocalizationByModel;
+use LaravelLang\Routes\Middlewares\LocalizationByParameter;
+use LaravelLang\Routes\Middlewares\LocalizationByParameterWithRedirect;
+use LaravelLang\Routes\Middlewares\LocalizationBySession;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
+        health: '/up',
+    )
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('queue:work --stop-when-empty')
+            ->everyMinute();
+    })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'localization.parameter' => LocalizationByParameter::class,
+            'localization.redirect' => LocalizationByParameterWithRedirect::class,
+            'localization.header' => LocalizationByHeader::class,
+            'localization.cookie' => LocalizationByCookie::class,
+            'localization.session' => LocalizationBySession::class,
+            'localization.model' => LocalizationByModel::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\LaravelBackupPanelServiceProvider::class,
+    App\Providers\MacrosServiceProvider::class,
+    Yajra\DataTables\DataTablesServiceProvider::class,
+    Yajra\DataTables\ButtonsServiceProvider::class,
+];
